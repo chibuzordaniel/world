fc646c7bd09967a86370d67a15270a5882e05b4c
chibuzordaniel <starchrischibuzor@gmail.com>
2021-08-26 23:30:20 +1300

    update file

---
+++ app.js
@@ -0,0 +1,67 @@
+// let name = prompt("hi");
+
+// document.getElementById('hello').innerHTML = 'hello:' +  name;
+
+// console.table([1, 2, 4, 66, ]);
+
+// let greet = "good",
+
+// output = greet.repeat('5');
+
+// console.log(output)
+
+//   const number1 = 30,
+//         number2 = 50,
+//         number3 = 70;
+
+//         let result;
+//         // addition
+
+//         result = number1 + number2;
+         
+//         result = number2 - number1;
+
+//         result = number1 / number2;
+
+//         result = number1 % number2;
+
+//         console.log(result);
+
+
+// console.log(1>2);
+// console.log(1 < 2)
+
+// const product = 'pizza',
+//       price = 30,
+//       product2 = 'Hamburger',
+//       price = 40;
+
+
+//       //old method
+//       let html;
+
+//      html = '<ul>' + 
+//              '<li>item:' + product1 + '</li>' +
+//              '<li>price:' + price1 + '</li>' +
+//              '<li>item:' + product2 + '</li>' +
+//              '<li>price:' + price2 + '</li>' +
+//              '<li>total:' + (price1 + price2) + '</li>' +
+//            '</ul>';
+
+//       let app = document.querySelector('#app')
+//       app.innerHTML =html
+
+
+// //Array of  months
+// const months = new Array('January',  'February',  'march',
+// 'April',  'May', 'June');
+
+// //check
+// console.log(months.length);
+
+
+// const today = new Date();
+
+// console.log(today)
+
+()
